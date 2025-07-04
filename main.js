352725d54450ebf77e25d5c66a7f2a8325fb5b64
Yasdiel Alejandro <bringasgonzalezy@gmail.com>
2025-07-04 19:08:23 -0400

    main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const listaPokemon = document.querySelector("#listaPokemon");
 const botonesHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
-for (let i = 1; i <= 151; i++) {
+for (let i = 1; i <= 1025; i++) {
     fetch(URL + i)
         .then((response) => response.json())
         .then(data => mostrarPokemon(data))
@@ -50,7 +50,7 @@ botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
 
     listaPokemon.innerHTML = "";
 
-    for (let i = 1; i <= 151; i++) {
+    for (let i = 1; i <= 1025; i++) {
         fetch(URL + i)
             .then((response) => response.json())
             .then(data => {
